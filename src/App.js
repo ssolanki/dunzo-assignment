76a2af80b18e5a66a6f89819fe64c00473529ff4
Sahil Solanki <sahilsolanki07@gmail.com>
2021-07-09 14:52:41 +0530

    copy

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Assignment
+        </p>
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
