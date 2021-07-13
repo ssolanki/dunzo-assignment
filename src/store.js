0042715306f003aaff7564f5c9c8a408441788ca
Sahil Solanki <sahilsolanki07@gmail.com>
2021-07-13 17:10:00 +0530

    thunk added

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
-// import thunk from "redux-thunk";
+import thunk from "redux-thunk"; // You can use other lib as well
 import rootReducer from "./reducers/rootReducer";
 export default function configureStore(initialState = {}) {
-	return createStore(rootReducer, initialState, applyMiddleware());
+	return createStore(rootReducer, initialState, applyMiddleware(thunk));
 }
