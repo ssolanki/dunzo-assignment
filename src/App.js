099c583c304572be3c642f18f92995f18695a8e9
ssolanki <sahilsolanki07@gmail.com>
2022-02-12 13:31:11 +0530

    first screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from "./logo.png";
 import "./App.css";
 import { connect } from "react-redux";
-import moment from "moment";
+// import moment from "moment";
+import placeHolderImg from "./assets/images/placeholder.png"
+
+// Note: use moment for formatting date like moment().format('LLLL')
 
 function App() {
 	return (
@@ -14,11 +17,9 @@ function App() {
 			<div className="Container">
 				<div className="App-Content">
 					<div className="Info">
-						<p>
-							TODO Assignment
-						</p>
-						<p>
-							Current time: {moment().format('LLLL')}
+						<img alt="placeholder" className="Placeholder" src={placeHolderImg} />
+						<p className="PlaceholderTitle">
+							Start creating your first task!
 						</p>
 					</div>
 				</div>
